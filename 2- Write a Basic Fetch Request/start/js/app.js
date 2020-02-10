e82e21374e6bb51cdcb0479a62b91226fc469a8c
Aziz Kemal HOSCAN <kemal.hoscan@yahoo.com>
2020-02-10 19:06:06 +0000

    fetch APi is almost done with some bugs to fix

---
+++ 2- Write a Basic Fetch Request/start/js/app.js
@@ -0,0 +1,106 @@
+const select = document.getElementById('breeds');
+const card = document.querySelector('.card');
+const form = document.querySelector('form');
+
+// ------------------------------------------
+//  FETCH FUNCTIONS
+function fetchData(url){
+  return fetch(url)
+    .then(checkStatus)
+    .then(response => response.json())
+    .catch(error => console.log("look likes there is a poblem", error))
+}
+'https://dog.ceo/api/breeds/image/random'
+Promise.all([
+  fetchData('https://dog.ceo/api/breeds/list'),
+  fetchData('https://dog.ceo/api/breeds/image/random')
+  ])
+.then(data => {
+  const breedList = data[0].message;
+  const imagelist = data[1].message;
+  generateOptions(breedList);
+  generateimage(imagelist);
+})
+// ------------------------------------------
+// Do not need below fetch codes since we have a Promise.all() method above
+// It handles both of them together simultaniously
+
+
+// fetchData('https://dog.ceo/api/breeds/list')
+//   // .then(response => response.json())
+//   .then(data => generateOptions(data.message))
+
+
+// fetchData('https://dog.ceo/api/breeds/image/random')
+//   // .then(response => response.json())
+//   .then(data => generateimage(data.message))
+
+
+// ------------------------------------------
+//  HELPER FUNCTIONS
+// ------------------------------------------
+
+
+function checkStatus(response){
+  // You do not need to equalize this to true since it is a value either true or false/
+  if(response.ok){
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
+function generateOptions(data){
+  console.log(data);
+  const options = data.map(item => {
+    `<option value='${item}'>${item}</option>`
+  }).join('');
+  select.innerHTML = options;
+}
+
+function generateimage(data){
+  const html = `
+  <img src='${data}' alt>
+  <p>Click to view images of ${select.value}s</p>
+  `
+  card.innerHTML = html;
+}
+
+function fetchBreedimage(){
+  const breed = select.value;
+  const img = card.querySelector('img');
+  const p = card.querySelector('p');
+    fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(data => {
+        img.src = data.message;
+        img.alt = breed;
+        p.textContent = `Click here for more photos of ${breed}s`;
+
+    })
+}
+console.log(select.value);
+// ------------------------------------------
+//  EVENT LISTENERS
+// ------------------------------------------
+select.addEventListener('change', fetchBreedimage)
+card.addEventListener('click', fetchBreedimage)
+form.addEventListener('submit', postData)
+
+
+// ------------------------------------------
+//  POST DATA
+// ------------------------------------------
+
+// Fake api post Checked by investigating json response back on the console.
+
+function postData(e){
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const comment = document.getElementById('comment').value;
+  fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(checkStatus)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+
